b05f57cb0932d7fd38b0323c9a4994845fe133cb
Allison Ferreira de Oliveira <allison.f.oliveira@hotmail.com>
2021-08-02 17:44:27 -0300

    rotacionando interface

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import { render } from "./objs/renderer.js";
 
 let cube = createCube(3);
 render(cube, document, "cube-area");
+
+document.getElementById("button-rotate-F").addEventListener("click", () => rotate('Z', 0, false))
+
+function rotate(axis, layer, anti) {
+    let blockElements = document.querySelectorAll(`.scene>.cube>.block.position-${axis}-${layer}`);
+    blockElements.forEach(_ => _.classList.add(`rotate-${axis}`));
+}
