54bd1633537ab4421eaa839510e819849f20b1cf
Allison Ferreira de Oliveira <allison.f.oliveira@hotmail.com>
2021-08-03 14:48:17 -0300

    codigo ajustado

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
-import { AXIS, CLOCK } from "./objs/constants.js";
 import { createCube } from "./objs/creator.js";
-import { render } from "./objs/renderer.js";
-import { rotateCube } from "./objs/rotator.js";
-import { axisToString, movementFromString } from "./objs/transformer.js";
-
-let isBusy = false;
-let rootName = "cube-area"
-let cube = createCube(3);
-render(cube, document, rootName);
+import { createScene, rotateScene } from "./objs/interface/scene.js";
+import { movementFromString } from "./objs/transformer.js";
 
 
+let scene = createScene(document, createCube(3));
+document.querySelector(".cube-area").appendChild(scene.sceneHtmlElement);
+
+let isBusy = false
 let buttons = document.querySelectorAll(".button-area>.button-rotate");
-buttons.forEach(_ => _.addEventListener("click", (event) => rotateInterface(event.target.value)));
-
-function rotateInterface(v) {
+buttons.forEach(_ => _.addEventListener("click", (event) => {
     if (isBusy) return;
     isBusy = true;
-    let movement = movementFromString(v);
-    let axis = axisToString(movement.axis);
-    let blockElements = [];
-    let layers = [];
-    if (movement.layers) {
-        layers = movement.layers.map(_ => _ < 0 ? cube.length + _ : _);
-    } else {
-        layers = [...Array(cube.length).keys()];
-    }
-    
-    for(let layer of layers) {
-        document.querySelectorAll(`.scene>.cube>.block.position-${axis}-${layer}`).forEach(_ => blockElements.push(_));
-    }
-    blockElements.forEach(_ => _.classList.add('rotate', `rotate-${axis}${movement.clock ? '' : '-anti'}`));
-    setTimeout(() => {
-        for(let layer of layers) {
-            cube = rotateCube(movement.axis, cube, layer, movement.clock);
-        }
-        render(cube, document, rootName);
-        isBusy = false;
-    }, 350);
-}
+    rotateScene(scene, movementFromString(event.target.value), () => isBusy = false)
+}));
+
